5f6490aa9b4ef3f6b74ed549b8897f0d0e37dedb
user7-web <user7@avyukti.com>
2026-03-25 13:47:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #This app.py is a simple Flask application that serves as a demonstration
 # for CI/CD to Google Kubernetes Engine (GKE).
 # It has two routes: the root route ("/") which returns a message indicating that CI/CD to GKE is working, and a health check route ("/health") which returns an "OK" status with a 200 HTTP status code. The application listens on all available interfaces on port 8080.
+#Test Comment
 from flask import Flask
  
 app = Flask(__name__)
